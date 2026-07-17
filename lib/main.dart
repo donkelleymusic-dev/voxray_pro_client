8e2db538bce2920960744bb13714985882f5ba1d
@don-music <donkelleymusic@gmail.com>
2026-07-16 21:22:32 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1538,7 +1538,7 @@ class VoxrayDAWState extends VoxrayDAWStateBase with DawAudioController, DawApiS
                 }
               },
               child: const Text('Throw Test Error'),
-            )
+            ),
             ListTile(
               leading: const Icon(Icons.multitrack_audio,
                   color: Colors.amberAccent, size: 28),
