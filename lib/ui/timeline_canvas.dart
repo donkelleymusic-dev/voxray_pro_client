07f2e838c334878ad51151e68c8a88963d2cd115
@don-music <donkelleymusic@gmail.com>
2026-07-19 08:13:35 -0700

    Update timeline_canvas.dart

--- lib/ui/timeline_canvas.dart
+++ lib/ui/timeline_canvas.dart
@@ -136,16 +136,16 @@ class _TimelineCanvasWidgetState extends State<TimelineCanvasWidget> with Single
       return 0.0;
     } else {
       
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('calculateAudioLevel error rmsEnvelope NOT empty'), backgroundColor: Colors.cyan),
-      );
+      //ScaffoldMessenger.of(context).showSnackBar(
+      //  SnackBar(content: Text('calculateAudioLevel error rmsEnvelope NOT empty'), backgroundColor: Colors.cyan),
+      )//;
     }
     int index = (currentPlayheadSeconds * 10).floor();
     if (index >= 0 && index < precomputedrmsEnvelope.length) {
       
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('calculateAudioLevel precomputedrmsEnvelope: $precomputedrmsEnvelope[index]'), backgroundColor: Colors.green),
-      );
+      //ScaffoldMessenger.of(context).showSnackBar(
+      //  SnackBar(content: Text('calculateAudioLevel precomputedrmsEnvelope: $precomputedrmsEnvelope[index]'), backgroundColor: Colors.green),
+      //);
       return precomputedrmsEnvelope[index];
     }
     return 0.0;
